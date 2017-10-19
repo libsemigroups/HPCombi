6589df343b924846471bb95ecdbf129cd009392d
Florent Hivert <Florent.Hivert@lri.fr>
2017-10-19 10:28:38 +0200

    Initial Rev

---
+++ demovect.cpp
@@ -0,0 +1,62 @@
+#include <x86intrin.h>
+#include <cassert>
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+/**********************************************************************/
+/************** Défnitions des types et convertisseurs ****************/
+/**********************************************************************/
+
+/** Variable vectorielle
+ * vecteur de 16 byte représentant une permutation
+ * supporte les commandees vectorielles du processeur
+ **/
+using perm = uint8_t __attribute__ ((vector_size (16), __may_alias__));
+
+
+/**********************************************************************/
+/***************** Fonctions d'affichages *****************************/
+/**********************************************************************/
+
+/** Affichage perm
+ * Définition de l'opérateur d'affichage << pour le type perm
+ **/
+ostream & operator<<(ostream & stream, perm const &p) {
+    stream << "[" << setw(2) << hex << unsigned(p[0]);
+    for (unsigned i=1; i < 16; ++i)
+        stream << "," << setw(2) << hex << unsigned(p[i]) << dec;
+    stream << "]";
+    return stream;
+}
+
+/**********************************************************************/
+/****** Permutations Variables globales et fonctions de base **********/
+/**********************************************************************/
+
+/** Permutation identité **/
+const perm permid {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
+/** Permutation décalée d'un cran à gauche **/
+const perm decal {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,15};
+
+int main() {
+  const perm v1 {2,1,  7,4,9,15,12,0,5,3,6,8,11,10,14,13};
+  const perm v2 {2,1,32,4,8,  1,12,0,4,4,4,4,41,10,14,13};
+  perm v3;
+  v3 = v1 <= v2;
+
+    cout << v1 << endl;
+    cout << v2 << endl;
+    cout << v3 << endl;
+
+    int b = _mm_movemask_epi8(v3);
+    cout << "Application du masque : on obtient un nombre dont les 1 binaires représentent les positions non nulles : " << hex << unsigned(b) << endl;
+    cout << "On compte les 1 avec une opération du processeur" << endl;
+    cout << _mm_popcnt_u32(b) << endl;
+
+}
+    
---
+++ perm32.cpp
@@ -0,0 +1,157 @@
+#include <chrono>
+#include <x86intrin.h>
+#include <cassert>
+#include <algorithm>
+#include <array>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+/**********************************************************************/
+/************** Défnitions des types et convertisseurs ****************/
+/**********************************************************************/
+
+/** Variable vectorielle
+ * vecteur de 16 byte représentant une permutation
+ * supporte les commandees vectorielles du processeur
+ **/
+using epu8 = int8_t __attribute__ ((vector_size (16), __may_alias__));
+using perm32 = std::array<epu8, 2>;
+
+inline int8_t &set(perm32 &p, uint64_t i) {
+  return *(&p[0][0] + i);
+}
+inline int8_t get(perm32 p, uint64_t i) {
+  return *(&p[0][0] + i);
+}
+/**********************************************************************/
+/***************** Fonctions d'affichages *****************************/
+/**********************************************************************/
+
+/** Affichage perm32
+ * Définition de l'opérateur d'affichage << pour le type perm32
+ **/
+ostream & operator<<(ostream & stream, perm32 const &p) {
+  stream << "[" << setw(2) << hex << unsigned(get(p, 0));
+  for (unsigned i=1; i < 32; ++i)
+    stream << "," << setw(2) << hex << unsigned(get(p, i)) << dec;
+  stream << "]";
+  return stream;
+}
+
+/**********************************************************************/
+/****** Permutations Variables globales et fonctions de base **********/
+/**********************************************************************/
+
+/** Permutation identité **/
+const perm32 permid {
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,
+  16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31
+    };
+
+/**********************************************************************/
+/************************ Utilitaires *********************************/
+/**********************************************************************/
+
+perm32 random_perm32() {
+    perm32 res = permid;
+    random_shuffle(&set(res, 0), &set(res, 32));
+    return res;
+}
+
+/** Construit un vecteurs d'ar16 au hasard
+ * @param sz le nombre d'élements
+ * @return le vecteur correspondant
+ **/
+std::vector<perm32> rand_perms(int sz) {
+  std::vector<perm32> res(sz);
+  std::srand(std::time(0));
+  for (int i = 0; i < sz; i++) res[i] = random_perm32();
+  return res;
+}
+
+
+
+/** Calcul et affiche le temps de calcul d'une fonction
+ * @param func la fonction à executer
+ * @param reftime le temps de référence
+ * @return le temps d'exécution
+ **/
+template<typename Func>
+double timethat(Func fun, double reftime = 0) {
+  std::chrono::high_resolution_clock::time_point tstart, tfin;
+
+  tstart = std::chrono::high_resolution_clock::now();
+  fun();
+  tfin = std::chrono::high_resolution_clock::now();
+
+  auto tm = std::chrono::duration_cast<
+    std::chrono::duration<double>>(tfin - tstart);
+  std::cout << "time = " << std::setprecision(3) << tm.count() << "s";
+  if (reftime != 0) std::cout << ", speedup = " << reftime/tm.count();
+  std::cout << std::endl;
+  return tm.count();
+}
+
+
+/**********************************************************************/
+/************************ Primitives  *********************************/
+/**********************************************************************/
+
+inline bool eqperm32(perm32 p1, perm32 p2) {
+  return (_mm_movemask_epi8(_mm_cmpeq_epi8(p1[0], p2[0])) == 0xffff) &
+         (_mm_movemask_epi8(_mm_cmpeq_epi8(p1[1], p2[1])) == 0xffff);
+}
+
+perm32 permute(const perm32 &v1, const perm32 &v2) {
+  return {_mm_blendv_epi8(_mm_shuffle_epi8(v1[1], v2[0]),
+			  _mm_shuffle_epi8(v1[0], v2[0]),
+			  v2[0] < 16),
+          _mm_blendv_epi8(_mm_shuffle_epi8(v1[1], v2[1]),
+			  _mm_shuffle_epi8(v1[0], v2[1]),
+			  v2[1] < 16)};
+}
+
+
+perm32 permute_ref(const perm32 &v1, const perm32 &v2) {
+  perm32 res;
+  for (uint64_t i=0; i<32; i++) set(res, i) = get(v1, get(v2, i));
+  return res;
+}
+
+
+int main() {
+  std::srand(std::time(0));
+  perm32 v1 = random_perm32();
+  perm32 v2 = random_perm32();
+  cout << permid << endl;
+  cout << v1 << endl;
+  cout << v2 << endl;
+  cout << permute(v1, v2) << endl;
+  cout << permute_ref(v1, v2) << endl;
+
+  cout << "Sampling : "; cout.flush();
+  auto vrand = rand_perms(10000000);
+  cout << "Done !" << endl;
+  std::vector<perm32> check_ref(vrand.size());
+  std::vector<perm32> check(vrand.size());
+
+  cout << "Ref :  ";
+  double sp_ref = timethat([&vrand, &check_ref]() {
+      for (unsigned int i=0; i < vrand.size(); i++)
+	check_ref[i] = permute_ref(vrand[i], vrand[i]);
+    }, 0.0);
+  cout << "Fast : ";
+  timethat([&vrand, &check]() {
+      for (unsigned int i=0; i < vrand.size(); i++)
+	check[i] = permute(vrand[i], vrand[i]);
+    }, sp_ref);
+  cout << "Checking : "; cout.flush();
+  for (unsigned int i=0; i < vrand.size(); i++)
+    assert(eqperm32(check_ref[i], check[i]));
+  cout << "Ok !" << endl;
+}
+
+
