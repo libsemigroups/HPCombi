7206a8ae2c4946a60608737b5ed2c267eb40ae2c
Daniel Vanzo <daniel.vanzo@lri.fr>
2018-01-31 10:52:07 +0100

    Installation benchmark dependencie first

--- ci/travis-benchmark-install.sh
+++ ci/travis-benchmark-install.sh
@@ -4,6 +4,21 @@ set -e
 echo "Git version:"
 git --version
 
+echo "installing gtest from sources"
+git clone https://github.com/google/googletest.git
+cd googletest
+mkdir build
+cd build
+cmake ..
+make
+cd ../
+sudo cp -r googletest/include/gtest /usr/local/include
+sudo cp build/googlemock/gtest/lib*.a /usr/local/lib
+sudo cp build/googlemock/lib*.a /usr/local/lib
+cd ../
+rm -rf googletest
+
+echo "installing benchmark from sources"
 git clone https://github.com/google/benchmark.git googlebenchmark
 cd googlebenchmark
 mkdir build
@@ -11,4 +26,5 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=RELEASE
 make
 sudo make install
+cd ../../
 rm -rf googlebenchmark
