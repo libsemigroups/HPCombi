8a621da8c2df9b3fcb3725417e81c9d2b33d7d46
Daniel Vanzo <daniel.vanzo@lri.fr>
2018-01-31 10:40:04 +0100

    Second try for installing benchmark with travis

--- ci/travis-benchmark-install.sh
+++ ci/travis-benchmark-install.sh
@@ -1,7 +1,14 @@
-git clone https://github.com/google/benchmark.git
-cd benchmark
+#!/bin/bash
+set -e
+
+echo "Git version:"
+git --version
+
+git clone https://github.com/google/benchmark.git googlebenchmark
+cd googlebenchmark
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=RELEASE
 make
 sudo make install
+rm -rf googlebenchmark
