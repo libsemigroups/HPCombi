398b09e2a04f37ccb63c903500c43f56f8b69241
James Mitchell <jdm3@st-and.ac.uk>
2018-05-10 15:54:33 +0100

    Avoid duplicate symbols when including perm16.hpp in multiple translation units
    The purpose of this commit is to avoid duplicate symbols at link time,
    when perm16.hpp is included in multiple implementation (*.cc/.cpp) files.
    Without these changes (or some equivalent changes) it is impossible to
    include HPCombi in libsemigroups because linking fails because of
    duplicate symbols for the static data members and member functions.

--- include/perm16.hpp
+++ include/perm16.hpp
@@ -20,7 +20,9 @@
 #include <cassert>
 #include <cstdint>
 #include <functional>  // less<>
+#include <iomanip>
 #include <ostream>
+#include <random>
 #include <x86intrin.h>
 
 #ifdef HPCOMBI_HAVE_CONFIG
@@ -55,8 +57,7 @@ template <class Function> HPCOMBI_CONSTEXPR epu8 make_epu8(Function f) {
   return make_epu8_helper(f, std::make_index_sequence<16>{});
 }
 
-template <uint8_t c> HPCOMBI_CONSTEXPR
-uint8_t constfun(uint8_t ignored) { return c; }
+template <uint8_t c> HPCOMBI_CONSTEXPR uint8_t constfun(uint8_t ) { return c; }
 
 template <uint8_t c> HPCOMBI_CONSTEXPR epu8 make_const_epu8() {
   return make_epu8_helper(constfun<c>, std::make_index_sequence<16>{});
@@ -218,14 +219,55 @@ struct alignas(16) Vect16 {
    */
   inline bool is_permutation(const size_t k = Size) const;
 
-  static Vect16 random(uint16_t bnd = 256);
+  static Vect16 random(uint16_t bnd = 256) {
+    Vect16 res;
+    std::random_device rd;
+
+    std::default_random_engine e1(rd());
+    std::uniform_int_distribution<int> uniform_dist(0, bnd - 1);
+    for (size_t i = 0; i < Size; i++)
+      res.v[i] = uniform_dist(e1);
+    return res;
+  }
 
 private:
-  static const std::array<Perm16, 9> sorting_rounds;
-  static const std::array<epu8, 4> summing_rounds;
+// Sorting network Knuth AoCP3 Fig. 51 p 229.
+  static constexpr std::array<epu8, 9> sorting_rounds = {{
+    //     0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15
+    epu8 { 1,  0,  3,  2,  5,  4,  7,  6,  9,  8, 11, 10, 13, 12, 15, 14},
+    epu8 { 2,  3,  0,  1,  6,  7,  4,  5, 10, 11,  8,  9, 14, 15, 12, 13},
+    epu8 { 4,  5,  6,  7,  0,  1,  2,  3, 12, 13, 14, 15,  8,  9, 10, 11},
+    epu8 { 8,  9, 10, 11, 12, 13, 14, 15,  0,  1,  2,  3,  4,  5,  6,  7},
+    epu8 { 0,  2,  1, 12,  8, 10,  9, 11,  4,  6,  5,  7,  3, 14, 13, 15},
+    epu8 { 0,  4,  8, 10,  1,  9, 12, 13,  2,  5,  3, 14,  6,  7, 11, 15},
+    epu8 { 0,  1,  4,  5,  2,  3,  8,  9,  6,  7, 12, 13, 10, 11, 14, 15},
+    epu8 { 0,  1,  2,  6,  4,  8,  3, 10,  5, 12,  7, 11,  9, 13, 14, 15},
+    epu8 { 0,  1,  2,  4,  3,  6,  5,  8,  7, 10,  9, 12, 11, 13, 14, 15}
+  }};
+#if defined(FF)
+#error FF is defined !
+#endif /* FF */
+#define FF 0xff
+
+  static constexpr std::array<epu8, 4> summing_rounds ={{
+    //      0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15
+    epu8 { FF,  0, FF,  2, FF,  4, FF,  6, FF,  8, FF, 10, FF, 12, FF, 14},
+    epu8 { FF, FF,  1,  1, FF, FF,  5,  5, FF, FF,  9,  9, FF, FF, 13, 13},
+    epu8 { FF, FF, FF, FF,  3,  3,  3,  3, FF, FF, FF, FF, 11, 11, 11, 11},
+    epu8 { FF, FF, FF, FF, FF, FF, FF, FF,  7,  7,  7,  7,  7,  7,  7,  7}
+  }};
+
+#undef FF
+public:
+  friend std::ostream &operator<<(std::ostream &stream, Vect16 const &term) {
+    stream << "[" << std::setw(2) << unsigned(term[0]);
+    for (unsigned i = 1; i < Vect16::Size; ++i)
+      stream << "," << std::setw(2) << unsigned(term[i]);
+    stream << "]";
+    return stream;
+  }
 };
 
-std::ostream &operator<<(std::ostream &stream, const Vect16 &term);
 
 
 /** Partial transformation of @f$\{0\dots 15\}@f$
@@ -341,8 +383,39 @@ struct Perm16 : public Transf16 {
   Perm16 left_shift_ff() { return make_epu8(make_left_shift_ff); }
 
   inline static Perm16 elementary_transposition(uint64_t i);
-  static Perm16 random();
-  static Perm16 unrankSJT(int n, int r);
+  static Perm16 random() {
+    Perm16 res = one();
+    std::random_shuffle(res.begin(), res.end());
+    return res;
+  }
+// From Ruskey : Combinatorial Generation page 138
+  static Perm16 unrankSJT(int n, int r) {
+    int j;
+    std::array<int, 16> dir;
+    Perm16 res{};
+    for (j = 0; j < n; ++j)
+      res[j] = 0xFF;
+    for (j = n - 1; j >= 0; --j) {
+      int k, rem, c;
+      rem = r % (j + 1);
+      r = r / (j + 1);
+      if ((r & 1) != 0) {
+        k = -1;
+        dir[j] = +1;
+      } else {
+        k = n;
+        dir[j] = -1;
+      }
+      c = -1;
+      do {
+        k = k + dir[j];
+        if (res[k] == 0xFF)
+          ++c;
+      } while (c < rem);
+      res[k] = j;
+    }
+    return res;
+}
 
   inline Vect16 lehmer_ref() const;
   inline Vect16 lehmer() const;
@@ -358,7 +431,13 @@ struct Perm16 : public Transf16 {
   inline uint8_t nb_cycles() const { return nb_cycles_unroll(); }
 
 private:
-  static const std::array<Perm16, 3> inverting_rounds;
+// Gather at the front numbers with (3-i)-th bit not set.
+  static constexpr std::array<epu8, 3> inverting_rounds = {{
+    //     0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15
+    epu8 { 0,  1,  2,  3,  8,  9, 10, 11,  4,  5,  6,  7, 12, 13, 14, 15},
+    epu8 { 0,  1,  4,  5,  8,  9, 12, 13,  2,  3,  6,  7, 10, 11, 14, 15},
+    epu8 { 0,  2,  4,  6,  8, 10, 12, 14,  1,  3,  5,  7,  9, 11, 13, 15}
+  }};
 };
 
 /*****************************************************************************/
--- include/perm16_impl.hpp
+++ include/perm16_impl.hpp
@@ -15,7 +15,6 @@
 
 #include "power.hpp"
 #include <algorithm>
-#include <iomanip>
 #include <random>
 
 #ifdef HAVE_EXPERIMENTAL_NUMERIC_LCM
@@ -27,7 +26,7 @@
 namespace HPCombi {
 
 // Definitions since previously *only* declared
-HPCOMBI_CONSTEXPR size_t Vect16::Size;
+// HPCOMBI_CONSTEXPR size_t Vect16::Size;
 
 /*****************************************************************************/
 /** Implementation part for inline functions *********************************/
@@ -39,17 +38,6 @@ inline Vect16::Vect16(std::initializer_list<uint8_t> il, uint8_t def) {
   auto &a = as_array();
   for (size_t i = il.size(); i < Size; ++i)
     a[i] = def;
-}
-
-Vect16 Vect16::random(uint16_t bnd) {
-  Vect16 res;
-  std::random_device rd;
-
-  std::default_random_engine e1(rd());
-  std::uniform_int_distribution<int> uniform_dist(0, bnd - 1);
-  for (size_t i = 0; i < Size; i++)
-    res.v[i] = uniform_dist(e1);
-  return res;
 }
 
 // Comparison mode for _mm_cmpestri
@@ -242,40 +230,7 @@ inline Transf16::operator uint64_t() const {
 }
 
 
-Perm16 Perm16::random() {
-  Perm16 res = one();
-  std::random_shuffle(res.begin(), res.end());
-  return res;
-}
 
-// From Ruskey : Combinatorial Generation page 138
-Perm16 Perm16::unrankSJT(int n, int r) {
-  int j;
-  std::array<int, 16> dir;
-  Perm16 res{};
-  for (j = 0; j < n; ++j)
-    res[j] = 0xFF;
-  for (j = n - 1; j >= 0; --j) {
-    int k, rem, c;
-    rem = r % (j + 1);
-    r = r / (j + 1);
-    if ((r & 1) != 0) {
-      k = -1;
-      dir[j] = +1;
-    } else {
-      k = n;
-      dir[j] = -1;
-    }
-    c = -1;
-    do {
-      k = k + dir[j];
-      if (res[k] == 0xFF)
-        ++c;
-    } while (c < rem);
-    res[k] = j;
-  }
-  return res;
-}
 
 inline Perm16 Perm16::elementary_transposition(uint64_t i) {
   assert(i < vect::Size);
@@ -331,9 +286,8 @@ template <> struct Monoid<Perm16> {
   static const Perm16 one;
   static Perm16 prod(Perm16 a, Perm16 b) { return a * b; }
 };
-
-const Perm16 power_helper::Monoid<Perm16>::one = Perm16::one();
-}  // namespace power_helper
+// const Perm16 Monoid<Perm16>::one = Perm16::one();
+};  // namespace power_helper
 
 inline Perm16 Perm16::inverse_cycl() const {
   Perm16 res = one();
@@ -432,55 +386,5 @@ inline uint8_t Perm16::nb_cycles_unroll() const {
   Vect16 res = one().v == cycles_mask_unroll().v;
   return _mm_popcnt_u32(_mm_movemask_epi8(res));
 }
-
-std::ostream &operator<<(std::ostream &stream, Vect16 const &term) {
-  stream << "[" << std::setw(2) << unsigned(term[0]);
-  for (unsigned i = 1; i < Vect16::Size; ++i)
-    stream << "," << std::setw(2) << unsigned(term[i]);
-  stream << "]";
-  return stream;
-}
-
-// clang-format off
-
-// Sorting network Knuth AoCP3 Fig. 51 p 229.
-const std::array<Perm16, 9> Vect16::sorting_rounds = {{
-    //     0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15
-    epu8 { 1,  0,  3,  2,  5,  4,  7,  6,  9,  8, 11, 10, 13, 12, 15, 14},
-    epu8 { 2,  3,  0,  1,  6,  7,  4,  5, 10, 11,  8,  9, 14, 15, 12, 13},
-    epu8 { 4,  5,  6,  7,  0,  1,  2,  3, 12, 13, 14, 15,  8,  9, 10, 11},
-    epu8 { 8,  9, 10, 11, 12, 13, 14, 15,  0,  1,  2,  3,  4,  5,  6,  7},
-    epu8 { 0,  2,  1, 12,  8, 10,  9, 11,  4,  6,  5,  7,  3, 14, 13, 15},
-    epu8 { 0,  4,  8, 10,  1,  9, 12, 13,  2,  5,  3, 14,  6,  7, 11, 15},
-    epu8 { 0,  1,  4,  5,  2,  3,  8,  9,  6,  7, 12, 13, 10, 11, 14, 15},
-    epu8 { 0,  1,  2,  6,  4,  8,  3, 10,  5, 12,  7, 11,  9, 13, 14, 15},
-    epu8 { 0,  1,  2,  4,  3,  6,  5,  8,  7, 10,  9, 12, 11, 13, 14, 15}
-  }};
-
-// Gather at the front numbers with (3-i)-th bit not set.
-const std::array<Perm16, 3> Perm16::inverting_rounds = {{
-    //     0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15
-    epu8 { 0,  1,  2,  3,  8,  9, 10, 11,  4,  5,  6,  7, 12, 13, 14, 15},
-    epu8 { 0,  1,  4,  5,  8,  9, 12, 13,  2,  3,  6,  7, 10, 11, 14, 15},
-    epu8 { 0,  2,  4,  6,  8, 10, 12, 14,  1,  3,  5,  7,  9, 11, 13, 15}
-  }};
-
-
-#if defined(FF)
-#error FF is defined !
-#endif /* FF */
-#define FF 0xff
-
-const std::array<epu8, 4> Vect16::summing_rounds = {{
-    //      0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15
-    epu8 { FF,  0, FF,  2, FF,  4, FF,  6, FF,  8, FF, 10, FF, 12, FF, 14},
-    epu8 { FF, FF,  1,  1, FF, FF,  5,  5, FF, FF,  9,  9, FF, FF, 13, 13},
-    epu8 { FF, FF, FF, FF,  3,  3,  3,  3, FF, FF, FF, FF, 11, 11, 11, 11},
-    epu8 { FF, FF, FF, FF, FF, FF, FF, FF,  7,  7,  7,  7,  7,  7,  7,  7}
-  }};
-
-#undef FF
-
-// clang-format on
 
 }  // namespace HPCombi
