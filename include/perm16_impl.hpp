1762b071ed71fa75d9b2ddd72172bd67cd193f05
Florent Hivert <Florent.Hivert@lri.fr>
2018-05-14 15:20:24 +0200

    Missing braces warning

--- include/perm16_impl.hpp
+++ include/perm16_impl.hpp
@@ -442,29 +442,29 @@ inline std::ostream &operator<<(std::ostream &stream, Vect16 const &term) {
 
 // Sorting network Knuth AoCP3 Fig. 51 p 229.
 const std::array<Perm16, 9> Vect16::sorting_rounds() {
-  static std::array<Perm16, 9> res {
+  static std::array<Perm16, 9> res {{
     //     0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15
     epu8 { 1,  0,  3,  2,  5,  4,  7,  6,  9,  8, 11, 10, 13, 12, 15, 14},
     epu8 { 2,  3,  0,  1,  6,  7,  4,  5, 10, 11,  8,  9, 14, 15, 12, 13},
-    epu8 { 4,  5,  6,  7,  0,  1,  2,  3, 12, 13, 14, 15,  8,  9, 10, 11},
+    epu8 { 4,  5,  6,  7,  0,  1,  2,  3, 12, 13, 14, 15,  8,  9, 10, 11} ,
     epu8 { 8,  9, 10, 11, 12, 13, 14, 15,  0,  1,  2,  3,  4,  5,  6,  7},
     epu8 { 0,  2,  1, 12,  8, 10,  9, 11,  4,  6,  5,  7,  3, 14, 13, 15},
     epu8 { 0,  4,  8, 10,  1,  9, 12, 13,  2,  5,  3, 14,  6,  7, 11, 15},
     epu8 { 0,  1,  4,  5,  2,  3,  8,  9,  6,  7, 12, 13, 10, 11, 14, 15},
     epu8 { 0,  1,  2,  6,  4,  8,  3, 10,  5, 12,  7, 11,  9, 13, 14, 15},
     epu8 { 0,  1,  2,  4,  3,  6,  5,  8,  7, 10,  9, 12, 11, 13, 14, 15}
-  };
+  }};
   return res;
 }
 
 // Gather at the front numbers with (3-i)-th bit not set.
 const std::array<Perm16, 3> Perm16::inverting_rounds() {
-  static std::array<Perm16, 3> res {
+  static std::array<Perm16, 3> res {{
     //     0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15
     epu8 { 0,  1,  2,  3,  8,  9, 10, 11,  4,  5,  6,  7, 12, 13, 14, 15},
     epu8 { 0,  1,  4,  5,  8,  9, 12, 13,  2,  3,  6,  7, 10, 11, 14, 15},
     epu8 { 0,  2,  4,  6,  8, 10, 12, 14,  1,  3,  5,  7,  9, 11, 13, 15}
-  };
+  }};
   return res;
 }
 
@@ -475,13 +475,13 @@ const std::array<Perm16, 3> Perm16::inverting_rounds() {
 #define FF 0xff
 
 const std::array<epu8, 4> Vect16::summing_rounds() {
-  static std::array<epu8, 4> res {
+  static std::array<epu8, 4> res {{
     //      0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15
     epu8 { FF,  0, FF,  2, FF,  4, FF,  6, FF,  8, FF, 10, FF, 12, FF, 14},
     epu8 { FF, FF,  1,  1, FF, FF,  5,  5, FF, FF,  9,  9, FF, FF, 13, 13},
     epu8 { FF, FF, FF, FF,  3,  3,  3,  3, FF, FF, FF, FF, 11, 11, 11, 11},
     epu8 { FF, FF, FF, FF, FF, FF, FF, FF,  7,  7,  7,  7,  7,  7,  7,  7}
-  };
+  }};
   return res;
 }
 
