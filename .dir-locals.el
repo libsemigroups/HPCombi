d21a05e0e93ffd58eee3815929bc65be010a312a
Florent Hivert <Florent.Hivert@lri.fr>
2018-08-03 01:22:29 +0200

    Added emacs dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil . ((eval . (setq default-directory
+                       (concat
+                        (locate-dominating-file buffer-file-name ".dir-locals.el")
+                        "build/")
+)))))
