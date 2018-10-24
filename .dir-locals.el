98a54d0952e8d48fdce400b6f20f0aed43a86811
Florent Hivert <Florent.Hivert@lri.fr>
2018-10-24 11:15:11 +0200

    Removed .dir-locals.el

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-((nil . ((eval . (setq default-directory
-                       (concat
-                        (locate-dominating-file buffer-file-name ".dir-locals.el")
-                        "build/")
-)))))
