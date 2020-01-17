06781daa91afb7b00a2ec054849bd18dd9ab687a
Florent Hivert <Florent.Hivert@lri.fr>
2020-01-17 16:33:32 +0100

    CI install benchmark from a fixed release (1.5.0)

--- ci/travis-benchmark-install.sh
+++ ci/travis-benchmark-install.sh
@@ -4,27 +4,26 @@ set -e
 echo "Git version:"
 git --version
 
-# echo "installing gtest from sources"
-# git clone https://github.com/google/googletest.git
-# cd googletest
+# curl -L -O https://github.com/google/googletest/archive/release-1.10.0.tar.gz
+# tar xvf release-1.10.0.tar.gz
+# cd googletest-release-1.10.0
 # mkdir build
 # cd build
-# cmake ..
+# cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DBENCHMARK_DOWNLOAD_DEPENDENCIES=ON
 # make
-# cd ../
-# sudo cp -r googletest/include/gtest /usr/local/include
-# sudo cp build/googlemock/gtest/lib*.a /usr/local/lib
-# sudo cp build/googlemock/lib*.a /usr/local/lib
-# cd ../
-# rm -rf googletest
+# sudo make install
+# cd ../../
+# rm -rf googletest-release-1.10.0
 
 echo "installing benchmark from sources"
-git clone https://github.com/google/benchmark.git googlebenchmark
-cd googlebenchmark
+curl -L -O https://github.com/google/benchmark/archive/v1.5.0.tar.gz
+tar xvf v1.5.0.tar.gz
+cd benchmark-1.5.0
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DBENCHMARK_DOWNLOAD_DEPENDENCIES=ON
+#cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DBENCHMARK_DOWNLOAD_DEPENDENCIES=ON
+cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DBENCHMARK_ENABLE_GTEST_TESTS=OFF -DBENCHMARK_ENABLE_TESTING=OFF
 make
 sudo make install
 cd ../../
-rm -rf googlebenchmark
+rm -rf benchmark-1.5.0
