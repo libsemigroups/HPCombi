ecfd8520624e769086721e71e3125ebf9d6fa22f
James D. Mitchell <jdm3@st-andrews.ac.uk>
2023-10-26 13:33:50 +0100

    Fixup stop using boost

--- tests/test_main.hpp
+++ tests/test_main.hpp
@@ -1,20 +1,17 @@
-//
-// libsemigroups - C++ library for semigroups and monoids
-// Copyright (C) 2019 James D. Mitchell
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
+//****************************************************************************//
+//       Copyright (C) 2023 James D. Mitchell <jdm3@st-andrews.ac.uk>         //
+//                                                                            //
+//  Distributed under the terms of the GNU General Public License (GPL)       //
+//                                                                            //
+//    This code is distributed in the hope that it will be useful,            //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of          //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       //
+//   General Public License for more details.                                 //
+//                                                                            //
+//  The full text of the GPL is available at:                                 //
+//                                                                            //
+//                  http://www.gnu.org/licenses/                              //
+//****************************************************************************//
 
 #ifndef HPCOMBI_TESTS_TEST_MAIN_HPP_
 #define HPCOMBI_TESTS_TEST_MAIN_HPP_
